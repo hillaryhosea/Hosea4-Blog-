862a2dbb1ea53093abe52d2c8b9fd7815e54ff0a
hillaryhosea <107868525+hillaryhosea@users.noreply.github.com>
2024-09-12 22:13:24 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import NavBar from "./NavBar";
+import Home from "./Home";
+
+function App() {
+  return (
+    <div className="App">
+        <NavBar />
+      <div className="component">
+      <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div className='navbar'>
+        <h1>Hosea4 Blog </h1>
+        <div className='links'>
+          <a href='/'>Home</a>
+          <a href=' /create'> New Blog </a>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar;
