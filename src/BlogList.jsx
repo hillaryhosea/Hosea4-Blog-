f364f5729f48439c515c608a8385014debee9a93
hillaryhosea <107868525+hillaryhosea@users.noreply.github.com>
2024-09-18 20:56:07 +0300

    Blog Delete

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function BlogList({blogs, Title}) {
+function BlogList({blogs, Title, handleDelete}) {
   return (
     <div className='blog-list'>
     <h2>{Title}</h2>
@@ -9,6 +9,7 @@ function BlogList({blogs, Title}) {
             <div className='blog-preview' key={blog.id}>
                 <h2>{ blog.Title}</h2>
                 <p>{blog.Author}</p>
+                <button onClick={() => handleDelete(blog.id)}> Delete Blog</button>
             </div>
         ))
     }
--- src/Home.jsx
+++ src/Home.jsx
@@ -3,15 +3,23 @@ import BlogList from './BlogList';
 
 function Home() {
 
-    const [blogs, setBlog] = useState([
-        {Title:"Welcome to my site", Author:" Hosea", Content:"lorem ipsum", id:1},
+    const [blogs, setBlogs] = useState([
+        {Title:"Welcome to my site", Author:"Hosea", Content:"lorem ipsum", id:1},
         {Title:"Article writing", Author:" Hillary", Content:"lorem ipsum", id:2},
-        {Title:"New progress update", Author:"Creag", Content:"lorem ipsum", id:3}
+        {Title:"New progress update", Author:"Hosea", Content:"lorem ipsum", id:3}
     ])
+
+    function handleDelete (id) {
+      const newBlogs = blogs.filter(blog => blog.id !== id);
+      setBlogs(newBlogs);
+    }
+
+   
 
   return (
     <div className='content'>
-    <BlogList blogs={blogs} Title="All Blogs"/>
+    <BlogList blogs={blogs} Title="All Blogs" handleDelete ={handleDelete}/>
+    {/* <BlogList blogs={blogs.filter((blog)=>blog.Author === "Hosea")} Title="Hosea Blogs"/> */}
     </div>
   )
 }
