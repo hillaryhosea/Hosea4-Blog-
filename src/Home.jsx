ae7794661022d4c3fd97521274a27327ec464ad6
hillaryhosea <107868525+hillaryhosea@users.noreply.github.com>
2024-10-02 22:01:32 +0300

    json data

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,25 +1,40 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import BlogList from './BlogList';
 
 function Home() {
 
-    const [blogs, setBlogs] = useState([
-        {Title:"Welcome to my site", Author:"Hosea", Content:"lorem ipsum", id:1},
-        {Title:"Article writing", Author:" Hillary", Content:"lorem ipsum", id:2},
-        {Title:"New progress update", Author:"Hosea", Content:"lorem ipsum", id:3}
-    ])
+    const [blogs, setBlogs] = useState([]);
+    const [error, setError] = useState(null);
 
     function handleDelete (id) {
       const newBlogs = blogs.filter(blog => blog.id !== id);
       setBlogs(newBlogs);
     }
 
+    useEffect (()=>{
+      const fetchData = async () =>{
+        
+        try{
+          const response = await fetch ("http://localhost:8000/blogs");
+          if (!response.ok){
+            throw new Error("response was not ok");
+            
+          }
+          const result = await response.json();
+          setBlogs(result);
+          
+        } catch (error){
+        setError(error)
+        }
+      }
+      
+      fetchData();
+    },[]);
    
 
   return (
     <div className='content'>
-    <BlogList blogs={blogs} Title="All Blogs" handleDelete ={handleDelete}/>
-    {/* <BlogList blogs={blogs.filter((blog)=>blog.Author === "Hosea")} Title="Hosea Blogs"/> */}
+   { blogs && <BlogList blogs={blogs} Title="All Blogs" handleDelete={handleDelete} /> }
     </div>
   )
 }
