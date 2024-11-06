1a2a6b79ea7451c56a0f3f37db6f728e6ab7858e
hillaryhosea <107868525+hillaryhosea@users.noreply.github.com>
2024-11-06 03:13:10 +0300

    making a post request

--- src/Creat.jsx
+++ src/Creat.jsx
@@ -1,7 +1,59 @@
+import { useState } from "react";
+import {useNavigate} from "react-router-dom"
 const Creat = () => {
+
+  const [title, setTitle] = useState('');
+  const [ body, setBody] = useState('');
+  const [ author, setAuthor] = useState ('Hosea');
+  const [isloading, setIsloading] = useState(false)
+  const navigate = useNavigate();
+  
+  function handleSubmit (e) {
+      e.preventDefault();
+      const blog = {title, body, author};
+      setIsloading(true)
+
+      fetch('http://localhost:8000/blogs', {
+        method:'POST',
+        headers:{'content-Type':'Application/json'},
+        body: JSON.stringify(blog)
+      }) .then(()=>{
+        console.log('new blog');
+        setIsloading(false)
+        navigate("/")
+      })
+      
+  }
+
   return (
     <div className="creat content">
       <h2>Add New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label> Blog Title</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle (e.target.value)}
+        />
+        <label>Blog Body</label>
+        <textarea
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+          required
+        />
+        <label>Blog Author</label>
+        <select
+          value={author}
+          onChange={(e)=> setAuthor(e.target.value)}
+        >
+          <option value="Hosea">Hosea</option>
+          <option value="Hillary">Hillary</option>
+        </select>
+        {!isloading && <button>Add Blog</button>}
+        {isloading && <button disabled>Adding blog....</button>}
+      </form>
+
     </div>
   );
 };
